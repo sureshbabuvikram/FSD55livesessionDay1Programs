eb0ea9a4eabb118e039feb5a827f89e6905f8c1c
sureshbabuvikram <sureshbabuvikram@gmail.com>
2024-03-04 13:20:53 +0530

    html

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app= express();
 const PORT=4000;   //http://localhost:4000
 
 app.get('/', (req,res)=>{
-    res.status(200).json({message:"Hai my dear team, we will become a FSD Developer"})
+    // res.status(200).json({message:"Hai my dear team, we will become a FSD Developer"})
+    res.status(200).send(`<div style=" background-color:yellow; color:blue " > <h1> Welcome to my team   </h1>   </div>`)
 })
 app.get('/get-data', (req,res)=>{
     res.status(200).json({message:"data", data:{name:"suresh"}})
