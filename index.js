ddaeda3f69e53e97edcf633619d49098b6519fd0
sureshbabuvikram <sureshbabuvikram@gmail.com>
2024-03-04 13:09:40 +0530

    timestamp

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import fs from 'fs';
+import {format} from 'date-fns';
 
 const app= express();
 const PORT=4000;   //http://localhost:4000
@@ -9,6 +11,18 @@ app.get('/', (req,res)=>{
 app.get('/get-data', (req,res)=>{
     res.status(200).json({message:"data", data:{name:"suresh"}})
 })
+
+app.get('/write-read', (req,res)=>{
+    let today= format(new Date(), 'dd-mm-yyyy-HH-mm-ss')
+    console.log(("today",today));
+    const filePath= `TimeStamp/${today}.txt`
+    fs.writeFileSync(filePath, `${today}` , 'utf8')
+    let data = fs.readFileSync(filePath, 'utf8')
+    res.status(200).send(data)
+})
+
+
+
 app.listen(PORT, ()=>{
     console.log(`App is running in the port  ${PORT}`);
 }) // running
